7dd1d52f327898cbdfe5cf91173ac7a020f6cf8f
bnmbz <michaelboateng064@gmail.com>
2023-08-27 02:27:00 +0000

    first commit

---
+++ assets/JS/main.js
@@ -0,0 +1,26 @@
+
+(function() {
+  "use strict";
+  
+  /**
+   * Mobile nav toggle
+   */
+  // on('click', '.mobile-nav-toggle', function(e) {
+  //   select('#navbar').classList.toggle('navbar-mobile')
+  //   this.classList.toggle('bi-list')
+  //   this.classList.toggle('bi-x')
+  // })
+
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+})()
